1cdc4eb1f7940449732fadce0496978120790fda
Chris Annunziato <NeonMeteors@gmail.com>
2024-08-28 13:13:47 -0400

    added light and dark mode in nav bar

--- src/Components/DarkLightButton.jsx
+++ src/Components/DarkLightButton.jsx
@@ -12,10 +12,18 @@ const DarkLightButton = () => {
     }, [darkMode]);
 
 
-    return (<button className="absolute w-16 h-16 bottom-16 right-16 bg-neutral-300 text-black
-            dark:bg-black rounded-full dark:text-white"
+    return (<button className="text-primary_text_light hover:text-primary_hover_light
+                dark:hover:text-primary_hover_dark dark:text-primary_text_dark "
                     onClick={toggleDarkMode}
-    >{darkMode ? "Dark" : "Light"}
+    >{darkMode ?
+        (
+        <svg className="fill-primary_icon_light dark:fill-primary_icon_dark
+                        hover:fill-primary_hover_light dark:hover:fill-primary_hover_dark" xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" viewBox="0 -960 960 960"><path d="M480-28 346-160H160v-186L28-480l132-134v-186h186l134-132 134 132h186v186l132 134-132 134v186H614L480-28Zm0-252q83 0 141.5-58.5T680-480q0-83-58.5-141.5T480-680q-83 0-141.5 58.5T280-480q0 83 58.5 141.5T480-280Zm0 140 100-100h140v-140l100-100-100-100v-140H580L480-820 380-720H240v140L140-480l100 100v140h140l100 100Z"/></svg>
+        ) : (
+        <svg className="fill-primary_icon_light dark:fill-primary_icon_dark translate-x-[-1px]
+                        hover:fill-primary_hover_light dark:hover:fill-primary_hover_dark" xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" viewBox="0 -960 960 960"><path d="M484-80q-84 0-157.5-32t-128-86.5Q144-253 112-326.5T80-484q0-146 93-257.5T410-880q-18 99 11 193.5T521-521q71 71 165.5 100T880-410q-26 144-138 237T484-80Z"/></svg>
+        )
+    }
     </button>)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import DarkLightButton from "./Components/DarkLightButton.jsx";
 import WaveUnderlay from "./Components/WaveUnderlay.jsx";
 import HeaderFooter from "./Components/HeaderFooter.jsx";
 
@@ -8,7 +7,6 @@ import HeaderFooter from "./Components/HeaderFooter.jsx";
 function App() {
     return (<div className={`relative h-screen bg-primary_light dark:bg-primary_dark font-moderustic text-lg`}>
         <WaveUnderlay/>
-        <DarkLightButton/>
         <HeaderFooter/>
     </div>)
 }
