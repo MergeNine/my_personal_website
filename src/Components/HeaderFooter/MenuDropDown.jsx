355072c02c315ad18dc5ab295d5a0819e562daef
Chris Annunziato <NeonMeteors@gmail.com>
2024-09-28 00:40:24 -0400

    added dropdown not complete

--- src/Components/HeaderFooter/MenuDropDown.jsx
+++ src/Components/HeaderFooter/MenuDropDown.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import NotesSvg from "../../Assets/Svg/SvgComponents/NotesSvg";
 import GithubSvg from "../../Assets/Svg/SvgComponents/GithubSvg";
 import LinkedinSvg from "../../Assets/Svg/SvgComponents/LinkedinSvg";
@@ -8,6 +8,22 @@ import MenuIconSvg from "../../Assets/Svg/SvgComponents/MenuIconSvg";
 
 const MenuDropDown = () => {
     const [open, setOpen] = useState(false)
+    const dropdownRef = useRef(null)
+
+    useEffect(() => {
+        const handleClick = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)){
+                setOpen(false)
+            }
+        }
+
+
+        document.addEventListener('mousedown', handleClick )
+        return () => {
+            document.removeEventListener(('mousedown', handleClick))
+        }
+    }, [])
+
 
     // const menuItem = [
     //     {icon: NotesSvg, label: "Notes"},
@@ -16,25 +32,35 @@ const MenuDropDown = () => {
     //     {icon: DarkButtonSvg, label: "Notes"},
     // ]
     //
-    return (
-        <div className="sm:hidden ">
-                <button onClick={() => setOpen(!open)} className="focus:outline-none block">
-                    <MenuIconSvg className="dark-light-fill h-[30px] w-[30px] sm:hidden"/>
-                </button>
-            {open ? (<div className="border absolute right-0 left-0 translate-y-1/2 -translate-x-1/2 mt-2">
-                    <ul>
-                        <li className="flex h-9 w-24 bg-amber-300">
-                            <div>Name</div>
-                            <div>Icon</div>
-                        </li>
-                    </ul>
-                </div>
-            ) : null}
+    return (<div ref={dropdownRef} className="relative sm:hidden ">
+            <button onClick={() => setOpen(!open)} className="focus:outline-none block">
+                <MenuIconSvg className=" dark-light-fill h-[30px] w-[30px] "/>
+            </button>
+            {open ? (<div className=" rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/4 -mt-1">
+                <ul className="flex flex-col gap-2">
+                    <li className="flex justify-center items-center gap-2 h-9 w-24 bg-amber-300 rounded-sm ">
+                        <div>Name</div>
+                        <GithubSvg/>
+                    </li>
+                    <li className="flex justify-center items-center gap-2 h-9 w-24 bg-amber-300 rounded-sm ">
+                        <div>Name</div>
+                        <GithubSvg/>
+                    </li>
+                    <li className="flex justify-center items-center gap-2 h-9 w-24 bg-amber-300 rounded-sm ">
+                        <div>Name</div>
+                        <GithubSvg/>
+                    </li>
+                    <li className="flex justify-center items-center gap-2 h-9 w-24 bg-amber-300 rounded-sm ">
+                        <div>Name</div>
+                        <GithubSvg/>
+                    </li>
+                </ul>
+            </div>) : null}
         </div>
 
     )
 }
-            export default MenuDropDown
+export default MenuDropDown
 
 
 
