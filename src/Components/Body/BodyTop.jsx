af2528ead127d6880906cc4bb2e8d21d2659fac9
Chris Annunziato <NeonMeteors@gmail.com>
2024-10-01 18:27:26 -0400

    updated copy

--- src/Components/Body/BodyTop.jsx
+++ src/Components/Body/BodyTop.jsx
@@ -1,69 +1,58 @@
 import Card from "./Card.jsx";
-import { createContext, useContext, useState } from "react";
+import {createContext, useContext, useState} from "react";
 import HoverTopSvg from "./HoverTopSvg.jsx";
 import Footer from "../HeaderFooter/Footer.jsx";
-import { useFullHoverContext } from "../Util/HoverContext.jsx";
+import {useFullHoverContext} from "../Util/HoverContext.jsx";
 import Header from "../HeaderFooter/Header.jsx";
 import profilePic from "../../Assets/Images/profilepic.jpg";
 
 const BodyTop = () => {
-  const { hoveredId, setHoveredId } = useFullHoverContext();
-  return (
-    <div className="bg-primary_light pt-12 dark:bg-primary_dark z-50 ">
-      <article className=" flex flex-col items-center justify-center gap-6 px-52 lg:min-w-[768px] lg:max-w-[1024px]">
-        <div className="mx-12 flex flex-col">
-          <div className="flex justify-between gap-10">
-            <div className="flex flex-col items-start justify-center">
-              <h2 className="text-3xl text-black dark:text-white">
-                Christopher Annunziato
-              </h2>
-              <p className="pl-1 text-black dark:text-white">
-                Fullstack React Java Developer
-              </p>
+    const {hoveredId, setHoveredId} = useFullHoverContext();
+    return (<div className="bg-primary_light pt-12 dark:bg-primary_dark z-50 ">
+        <article
+            className=" flex flex-col items-center justify-center gap-6 px-52 lg:min-w-[768px] lg:max-w-[1024px]">
+            <div className="mx-12 flex flex-col">
+                <div className="flex justify-between gap-10">
+                    <div className="flex flex-col items-start justify-center">
+                        <h2 className="text-3xl text-black dark:text-white">
+                            Christopher Annunziato
+                        </h2>
+                        <p className="pl-1 text-black dark:text-white">
+                            Fullstack React Java Developer
+                        </p>
+                    </div>
+                    <img
+                        className="h-28 w-28 rounded-full border-2 border-gray-100 object-cover"
+                        src={profilePic}
+                    ></img>
+                </div>
+
+                <div className="mt-2 flex flex-col gap-y-4 px-2  font-[600] text-black dark:text-neutral-300">
+                    <p className="flex flex-col gap-2 pr-14">
+                        <span>I'm a Philadelphia-based web developer currently working on an exciting idea:</span>
+                        <span>A fractal and algorithm path visualizer.</span>
+                        <span>As a former chemist, I'm interested in nature and its imperfections, as well as layering ideas and experimenting with web technology.</span>
+                        <span>My goal is to find the shortest path through several imperfect fractals (those with omissions, additions, and cross-linkages) using search algorithms.</span>
+                    </p>
+                </div>
             </div>
-            <img
-              className="h-28 w-28 rounded-full border-2 border-gray-100 object-cover"
-              src={profilePic}
-            ></img>
-          </div>
+        </article>
 
-          <div className="mt-2 flex flex-col gap-y-4 px-2  font-[600] text-black dark:text-neutral-300">
-            <p className="">
-              I'm a Philadelphia based web developer.
-            </p>
+        {/*<div className="ml-12 flex flex-col justify-start">*/}
+        {/*  <section className="text-primary_text_light dark:text-primary_text_dark">*/}
+        {/*    <div className="mr-24 flex h-[60px] items-center justify-between">*/}
+        {/*      <h1 className="text-4xl">Selected Work</h1>*/}
+        {/*      {hoveredId && <HoverTopSvg id={hoveredId} />}*/}
+        {/*    </div>*/}
+        {/*  </section>*/}
+        {/*  <div className="0 relative -ml-12 mt-6 flex h-72 justify-center gap-x-2">*/}
+        {/*    <Card name="Hello" id={1} />*/}
+        {/*    <Card name="Hell" id={2} />*/}
+        {/*    <Card name="He" id={3} />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
 
-            <p className="">
-              I'm currently working on an idea im very excited about.
-              <br/>
-              A fractal and algorithm path visualizer.
-            </p>
-            <p className="">
-             As a former chemist I'm interested in nature and its imperfections and in general,
-              layering ideas and experimenting with web technology.
-            </p>
-            <p className="">
-              I would like to find the shortest path of several imperfect fractals (those with omissions, additions and cross-linkages) via search algorithms.
-            </p>
-          </div>
-        </div>
-      </article>
-
-      {/*<div className="ml-12 flex flex-col justify-start">*/}
-      {/*  <section className="text-primary_text_light dark:text-primary_text_dark">*/}
-      {/*    <div className="mr-24 flex h-[60px] items-center justify-between">*/}
-      {/*      <h1 className="text-4xl">Selected Work</h1>*/}
-      {/*      {hoveredId && <HoverTopSvg id={hoveredId} />}*/}
-      {/*    </div>*/}
-      {/*  </section>*/}
-      {/*  <div className="0 relative -ml-12 mt-6 flex h-72 justify-center gap-x-2">*/}
-      {/*    <Card name="Hello" id={1} />*/}
-      {/*    <Card name="Hell" id={2} />*/}
-      {/*    <Card name="He" id={3} />*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-    </div>
-  );
+    </div>);
 };
 
 export default BodyTop;
