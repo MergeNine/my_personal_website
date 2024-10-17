b6e8983f195b06ffa23afaf6a088fc85ea7eb6c4
Chris Annunziato <NeonMeteors@gmail.com>
2024-10-17 15:25:27 -0400

    added postcssconfig back

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
@@ -8,5 +9,8 @@ export default defineConfig({
     },
     build: {
         outDir: 'dist'
-    }
+    },
+    css: {
+        postcss: './postcss.config.js',
+    },
 })
