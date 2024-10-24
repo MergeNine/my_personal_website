20a079cd192882cbf168193ab59ae573e915fd89
Chris Annunziato <NeonMeteors@gmail.com>
2024-10-24 16:16:17 -0400

    adjusted the width in bio

--- src/Components/Body/Bio.jsx
+++ src/Components/Body/Bio.jsx
@@ -12,35 +12,35 @@ const Bio = () => {
             border-light_accent dark:border-dark_accent ">Bio</span>
                 </div>
 
-                <p className="gap-2  font-normal border  max-w-[420px] sm:max-w-[435px]
+                <p className="font-normal max-w-[420px] sm:max-w-[435px]
         text-primary_text_light dark:text-primary_text_dark">
                     <ul>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">1989</div>
+                            <div className="font-semibold w-16">1989</div>
                             <div>Born in Media, PA</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">2012</div>
+                            <div className="font-semibold w-16">2012</div>
                             <div>B.S.Chemistry at Widener</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20 ">2017</div>
+                            <div className="font-semibold w-16 ">2017</div>
                             <div>M.S. in Chemistry at UPenn</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">2019</div>
+                            <div className="font-semibold w-16">2019</div>
                             <div>Chemist in Drug-Discovery</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">2022</div>
+                            <div className="font-semibold w-16">2022</div>
                             <div>Android Development</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">2024</div>
+                            <div className="font-semibold w-16">2024</div>
                             <div>Web Development</div>
                         </li>
                         <li className="flex items-start mb-1">
-                            <div className="font-semibold w-20">Pres.</div>
+                            <div className="font-semibold w-16">Pres.</div>
                             <div className="">React · Tailwind · Java · Spring</div>
                         </li>
                     </ul>
