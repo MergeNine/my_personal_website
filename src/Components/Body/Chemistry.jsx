b7aad4d67c980c8f184d536d103dd2e42de1bd4b
Chris Annunziato <NeonMeteors@gmail.com>
2024-10-24 15:30:57 -0400

    removed the no wrap on bio

--- src/Components/Body/Chemistry.jsx
+++ src/Components/Body/Chemistry.jsx
@@ -10,7 +10,7 @@ const Chemistry = () => {
             border-light_accent dark:border-dark_accent text-lg sm:text-xl ">Showcase</span>
             </div>
 
-            <p >Before I programmed web apps, I made molecules such as these, while working at Bristol Myers Squibb.</p>
+            <p>Before I programmed web apps, I made molecules such as these, while working at Bristol Myers Squibb.</p>
         </div>
 
         <div className="flex flex-col">
